fb5751968d5ef0571680b61c1c86284961b1e662
ardi <ardi.nusawan13@gmail.com>
2015-10-04 18:36:55 +0700

    Sudah bisa jalan servernya

--- Tugas_1/Server/src/server/Server.java
+++ Tugas_1/Server/src/server/Server.java
@@ -6,7 +6,10 @@
 package server;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,6 +28,22 @@ public class Server {
             ServerSocket servsock = new ServerSocket(1234);
             System.out.println("Server mau berjalan");
             Socket socket = servsock.accept();
+            System.out.println("Server berjalan ...");
+            InputStream is = socket.getInputStream();
+            OutputStream os = socket.getOutputStream();
+            
+            byte[] buf = new byte[50];
+            is.read(buf);
+            System.out.println(new String (buf));
+            os.write("selamat datang di serverku\r\n".getBytes());
+            os.flush();
+            
+            os.close();
+            is.close();
+            socket.close();
+            servsock.close();
+            
+            
         } catch (IOException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
