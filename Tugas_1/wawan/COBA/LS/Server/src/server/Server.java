dabc42ed7936c331d6f8f8f2239e724b8bccab44
ardi <ardi.nusawan13@gmail.com>
2015-10-13 10:09:40 +0700

    Tinggal CD

--- Tugas_1/wawan/COBA/LS/Server/src/server/Server.java
+++ Tugas_1/wawan/COBA/LS/Server/src/server/Server.java
@@ -41,10 +41,13 @@ public class Server {
             
             
             //if ls
-            //String ls = "ls";
-            //System.out.println(new String(buf));
-            //if(buf.toString()==ls)
-            //{
+            String ls = "ls";
+            //System.out.println("test " + new String(buf));
+            String buf2 = new String(buf);
+            buf2=buf2.trim();
+            if(buf2.equals(ls))
+            {
+           // { System.out.println("HAHAHAHAHAHAHAHAHAHHAHAHAHA"); }
                 
             String command = "ls /home/ardi/Downloads";
 
@@ -68,12 +71,67 @@ public class Server {
                 Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
             }
             
-            //}
+            }
             
-           // else 
-            //{
-           // System.out.println("sad");
-            //}
+            else 
+            {
+            //System.out.println("sad");
+                String mkdir = "mkdir";
+                String cd = "cd";
+                String buf3 = new String(buf);
+                buf3=buf3.trim();
+                String[] parts = buf3.split(" ");
+                
+                String part1 = parts[0];
+                String part2 = parts[1];
+            
+                if (part1.equals(mkdir))
+                {
+                    //System.out.println(part2);
+                    //String command = new String (buf);
+
+                    Process proc2 = Runtime.getRuntime().exec("mkdir " + part2);
+                    //System.out.println("Folder " + part2 + " has been created");
+                     String output1 = "Folder ";
+                     String output2 = new String(part2);
+                     String output3 = " has been created";
+                     String out_final = output1 + output2 + output3;
+                     System.out.println(out_final);
+                    // Read the output
+                       
+                    BufferedReader reader =  
+                      new BufferedReader(new InputStreamReader(proc2.getInputStream()));
+
+                    String line = "";
+                    while((line = reader.readLine()) != null) {
+                    //System.out.print(line + "\n");
+                       os.write(line.getBytes());
+
+                    }
+
+                    try {   
+                        proc2.waitFor();
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    
+                
+                }
+                else if(part1.equals(cd))
+                {                    
+                    //System.out.println("cd " + part2);
+                    //Runtime.getRuntime().exec("sh -c 'cd /home/ardi/wawan2 && mkdir ww");
+                    
+                    
+
+                }
+                
+            
+            }
+            //
+            
+            
             
             
             
