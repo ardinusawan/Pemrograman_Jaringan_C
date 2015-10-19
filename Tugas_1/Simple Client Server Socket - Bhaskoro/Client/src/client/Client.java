f89cbf1c5eb75814aa9b2af6dc9dde877a1f9fca
ardi <ardi.nusawan13@gmail.com>
2015-10-20 02:39:41 +0700

    Server - Client bisa berkomunikasi secara bergantian. Dimulai dari Client lalu diajwab oleh Server. Lalu kembali Client mengirim pesan, dst..

--- Tugas_1/Simple Client Server Socket - Bhaskoro/Client/src/client/Client.java
+++ Tugas_1/Simple Client Server Socket - Bhaskoro/Client/src/client/Client.java
@@ -5,11 +5,12 @@
  */
 package client;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,87 +25,41 @@ public class Client {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-       String msg;
+       
         try {
-            byte[] buf = new byte[1000];
             Socket socket = new Socket("localhost",1234);
-
             InputStream is = socket.getInputStream();
             OutputStream os = socket.getOutputStream();
-            //kita kerja dari sini
-            //baca config, masukin ke var LFolder
-            //deklarasi string lfolder
-            String LFolder = "/Users/dhanarp/random",inputString;
-            int inputInt;
-            Scanner s= new Scanner(System.in);
-
-            for(;;)
+            int count = 1;
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            String pesan;
+            
+            String message;
+            do
             {
-                System.out.println("Selamat datang di serverku");
-                System.out.println("pwd = "+ LFolder);
-                System.out.println("Command:");
-                System.out.println("1. List Directory");
-                System.out.println("2. Create new Directory");
-                System.out.println("3. Change Directory"); 
-                System.out.println("else = Exit Program");
-                inputInt=s.nextInt();                              //cara input int
-                //LFolder=s.next();                               //cara input string
-
-                if(inputInt==1)
-                {
-                    //kirim integer 1, lalu string blank
-                    msg="1"+LFolder;
-                    os.write(msg.getBytes());
-                }
-                else if(inputInt==2)
-                {
-                    System.out.println("Masukkan nama directory baru= ");
-                    inputString=s.next();
-                    LFolder = LFolder + inputString;
-                    msg="2"+LFolder;
-                    os.write(msg.getBytes());
-                    //kirim integer 2, sama LFolder
-                }
-                else if(inputInt==3)
-                {
-                    System.out.println("Masukkan nama directory yang dituju (full path)");
-                    System.out.println("Default= /Users/dhanarp/random/");
-                    inputString=s.next();
-                    LFolder = inputString;    //cuma ganti LFolder
-
-                }
-                else
-                {
-                    msg="0";
-                    os.write(msg.getBytes());
-                    break;
-                }    
-                //receive data
-                int len;
-                while(true)
-                {
-                    buf = new byte[1000];
-                    len = is.read(buf);
-                    if(len == -1)
-                    {
-                        break;
-                    }
-                    System.out.print(new String(buf));
-                }
-                //proses
-                //print
-            }
-
-
-            //kita kerja sampai sini
-            os.close();
-            is.close();
-
-
+            pesan = input.readLine();
+            os.write(pesan.getBytes());
+            os.flush();
+            
+            
+            byte[] buf = new byte[50];
+            
+            is.read(buf);
+                System.out.println(new String(buf));
+                message = new String(buf); 
+                //System.out.println("pesan" + pesan);
+                //os.flush();
+            
+            }while (!message.equals("QUIT"));
+            //os.close();
+            //is.close();
+            //socket.close();
+              
+            
         } catch (IOException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+    
     }
-
+    
 }
--- Tugas_1/Simple Client Server Socket - Bhaskoro/Server/src/server/Server.java
+++ Tugas_1/Simple Client Server Socket - Bhaskoro/Server/src/server/Server.java
@@ -5,9 +5,13 @@
  */
 package server;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -19,109 +23,47 @@ import java.util.logging.Logger;
  * @author ardi
  */
 public class Server {
-    //fungsi2
-    public static void lordMKDIR(String input)
-    {
-        File theDir = new File(input);
-        if (!theDir.exists()) 
-        {
-            System.out.println("creating directory: " + input);           //gaperlu
-            boolean result = false;
 
-            try{
-                theDir.mkdir();
-                result = true;
-            } 
-            catch(SecurityException se){
-                //handle it
-            }        
-            if(result) {    
-                System.out.println("DIR created");                      //print ke file output
-            }
-        }
-    }
-    public static void lordLS(final File folder) 
-    {
-        for (final File fileEntry : folder.listFiles()) 
-        {
-            System.out.println(fileEntry.getName());                    //nanti kirim ke file output
-        }
-    }
-
-    //end of fungsi2
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-
-        byte[] buf = new byte[1000];
-        String msg,msgCode;
-        int code;
-        File folder = new File("/Users/dhanarp/");
-
-
         try {
             // TODO code application logic here
             ServerSocket servsock = new ServerSocket(1234);
-            System.out.println("Server mau berjalan");
+            //System.out.println("Server mau berjalan");
             Socket socket = servsock.accept();
-            System.out.println("Server berjalan ...");
             InputStream is = socket.getInputStream();
-            OutputStream os = socket.getOutputStream();         //connected to client
-            //listen message
-            //kita kerja disini
-            //looping start
-
-           //kirim pesan
-            int len;
-            while(true)
+            OutputStream os = socket.getOutputStream();
+            
+            //int count = 1;
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            String pesan;
+            do
             {
-                //recv from client
-                buf = new byte[1000];
-                len = is.read(buf);
-                if(len == -1)
-                {
-                    break;
-                }
-                msg =new String(buf);
-                //end of recv. parsing.
-                msgCode=msg.substring(0, 1);
-                code= Integer.parseInt(msgCode);
-                //masuk if
-                if(code==1) //LS
-                {
-                    msg=msg.substring(1);
-                    folder = new File(msg);
-                    lordLS(folder);
-                    //edit lordLS sehingga hasil di print di File
-                    //kirim File lewat Socket
-                }
-                else if(code==2) //mkdir
-                {
-                    msg=msg.substring(1);
-                    lordMKDIR(msg);
-                    //harusnyajalan
-
-                }
-                //output ke file
-                //kirim file
-                else if(code==0)        break;
-
-            }
-            //end of kirim pesan
-
-
-
-
-
-            //end of looping
-            //kita kerja cuma sampai sini
-            os.close();
-            is.close();
-            socket.close();
-            servsock.close();
-
-
+            //System.out.println("Server berjalan ...");
+            byte[] buf = new byte[50];
+            is.read(buf);
+            //System.out.println("Test ...");
+            System.out.println(new String (buf));
+            pesan = input.readLine();
+            //pesan = pesan + "\n";
+            os.write(pesan.getBytes());
+            //os.write("selamat datang di serverku\r\n".getBytes());
+            os.flush();
+           // count++;
+            }while (!pesan.equals("QUIT"));
+            
+             //os.close();
+           // is.close();
+           // socket.close();
+            
+            //servsock.close();
+            
+            
         } catch (IOException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
+    
 }
